143a709a0c0f6ae10f078967d24af82d03c05ca2
martin0b101 <martin.vrbancic@gmail.com>
2022-03-17 22:07:05 +0100

    kocano

--- labwork03/script.js
+++ labwork03/script.js
@@ -1,14 +1,29 @@
 "use strict";
+let id = 1;
 
 function domRemoveParticipant(event) {
     let nameOfPerson = event.firstElementChild.innerHTML;
     let textForDelete = "Are you sure u want to delete " + nameOfPerson +"?" ;
+    let tableLocalStorage = JSON.parse(localStorage.getItem("participants"));
+    
     if (confirm(textForDelete) == true) {
+        //find element to delete in localStorage
+        for (let i = 0; i<tableLocalStorage.length; i++){
+            if (tableLocalStorage[i].first == nameOfPerson) {
+                tableLocalStorage.splice(i, 1);
+                console.log(tableLocalStorage);
+            }
+        }
+        //update local storage
+        localStorage.setItem("participants", JSON.stringify(tableLocalStorage));
         event.remove(); 
+        id -= 1;
     }
     
     //localStorage.removeItem(event);
 }
+
+
 
 function domAddParticipant(participant) {
     const table = document.querySelector("#participant-table");
@@ -20,11 +35,11 @@ function domAddParticipant(participant) {
     });
     
     for (const key in participant){
-        const td = document.createElement("td");
-        td.innerText = participant[key];
-        
-        tr.appendChild(td);
-        
+        if (key !== 'id') {
+            const td = document.createElement("td");
+            td.innerText = participant[key];
+            tr.appendChild(td);
+        }
     }
     
     
@@ -37,18 +52,31 @@ function addParticipant(event) {
     const role = document.querySelector("#role").value;
     
     
-    
     document.querySelector("#first").value = "";
     document.querySelector("#last").value = "";
+
+
     // Create participant object
     const participant = {
         first: first,
         last: last,
-        role: role
+        role: role,
+        id: id
     };
 
-    localStorage.setItem("participant", JSON.stringify(participant));
+    const arrayForLocalStroga = [participant];
+    if (localStorage.getItem('participants') ==null) {
+        localStorage.setItem("participants", JSON.stringify(arrayForLocalStroga));
+    }
+    else{
+        let dataInLocalStorage = JSON.parse(localStorage.getItem("participants"));
+        console.log(dataInLocalStorage);
+        dataInLocalStorage.push(participant);
+        localStorage.setItem('participants', JSON.stringify(dataInLocalStorage));
+    }
 
+    console.log(participant.id);
+    id += 1;
     // Add participant to the HTML
     domAddParticipant(participant);
 
@@ -57,7 +85,16 @@ function addParticipant(event) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
     document.getElementById("addButton").onclick = addParticipant;
+    if (localStorage.getItem('participants') !== null) {
+        //dodaj v tabelo 
+        let tableOfParticipants = JSON.parse(localStorage.getItem('participants'));
+        for (let i = 0; i<tableOfParticipants.length; i++){
+            domAddParticipant(tableOfParticipants[i]); 
+        }
+    }
+    
     //document.body.addEventListener("dblclick", domRemoveParticipant(window.ondblclick));
     
 
